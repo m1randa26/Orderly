55548f93204388a76051497505284e1aa95ba0da
GAreeem <20223tn084@utez.edu.mx>
2024-11-29 13:15:58 -0600

    Ganancias
    pantalla ganancias

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import Ganancias from './components/Ganancias/Ganancias.jsx'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
