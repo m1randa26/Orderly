deaa41719bf5963997094ebe8221c4bf1d9e0398
Alexssandro017 <133275970+Alexssandro017@users.noreply.github.com>
2024-11-30 18:37:40 -0600

    Main-Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
+// src/main.jsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
+import AppRouter from './Router.jsx';  // Importamos el Router
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -14,7 +15,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <App />
+      <AppRouter /> {/* Usamos el Router en lugar de Login directamente */}
     </ThemeProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
